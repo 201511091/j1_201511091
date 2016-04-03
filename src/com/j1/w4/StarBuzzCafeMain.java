e8d19217b7d467809423a4d22a444ffec1d73586
Eunho Kim <ster8@naver.com>
2016-04-03 16:01:58 +0900

    caramel macchiato added

--- src/com/j1/w4/StarBuzzCafeMain.java
+++ src/com/j1/w4/StarBuzzCafeMain.java
@@ -1,5 +1,17 @@
 package com.j1.w4;
 
+public class StarBuzzCafeMain {
+  public static void main(String[] args) {
+    Beverage dr = new DarkRoast();
+    Mocha m_dr = new Mocha(dr);
+    Whip w_m_dr = new Whip((Beverage)m_dr);
+    System.out.println(w_m_dr.getDescription());
+    System.out.println(w_m_dr.cost());
+    CaramelMacchiato cm = new CaramelMacchiato();
+    System.out.println(cm.getDescription());
+  }
+}
+
 abstract class Beverage {
   String description = "Unkown beverage";
   public String getDescription() {
@@ -14,6 +26,21 @@ class Espresso extends Beverage {
   }
   public double cost() {
     return 1.99;
+  }
+}
+
+class CaramelMacchiato extends Beverage {
+  Beverage temp;
+  public CaramelMacchiato() {
+    Beverage es = new Espresso();
+    Beverage es_m = new Milk(es);
+    Beverage es_m_s = new Syrup(es_m);
+    Beverage es_m_s_c = new CaramelSauce(es_m_s);
+    temp = es_m_s_c;
+    description = temp.getDescription() + " = Caramel Macchiato";
+  }
+  public double cost() {
+    return temp.cost();
   }
 }
 
@@ -56,12 +83,55 @@ class Whip extends CondimentDecorator {
   }
 }
 
-public class StarBuzzCafeMain {
-  public static void main(String[] args) {
-    Beverage dr = new DarkRoast();
-    Mocha m_dr = new Mocha(dr);
-    Whip w_m_dr = new Whip((Beverage)m_dr);
-    System.out.println(w_m_dr.getDescription());
-    System.out.println(w_m_dr.cost());
+class Ice extends CondimentDecorator {
+  Beverage beverage;
+  public Ice(Beverage b){
+    beverage = b;
+  }
+  public String getDescription() {
+    return beverage.getDescription() + ", Ice";
+  }
+  public double cost() {
+    return beverage.cost() + .0;
   }
 }
+
+class Milk extends CondimentDecorator {
+  Beverage beverage;
+  public Milk(Beverage b){
+    beverage = b;
+  }
+  public String getDescription() {
+    return beverage.getDescription() + ", Milk";
+  }
+  public double cost() {
+    return beverage.cost() + .50;
+  }
+}
+
+class Syrup extends CondimentDecorator {
+  Beverage beverage;
+  public Syrup(Beverage b){
+    beverage = b;
+  }
+  public String getDescription() {
+    return beverage.getDescription() + ", Syrup";
+  }
+  public double cost() {
+    return beverage.cost() + .20;
+  }
+}
+
+class CaramelSauce extends CondimentDecorator {
+  Beverage beverage;
+  public CaramelSauce(Beverage b){
+    beverage = b;
+  }
+  public String getDescription() {
+    return beverage.getDescription() + ", Caramel Sauce";
+  }
+  public double cost() {
+    return beverage.cost() + .40;
+  }
+}
+
