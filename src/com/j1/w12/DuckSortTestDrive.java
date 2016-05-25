54d4e6c16a714f4887294dee2f0829b3f7f41565
B-Sus <ster8@naver.com>
2016-05-25 12:44:47 +0900

    week12 reup

--- src/com/j1/w12/DuckSortTestDrive.java
+++ src/com/j1/w12/DuckSortTestDrive.java
@@ -7,9 +7,29 @@ public class DuckSortTestDrive {
   public static void main(String[] args) {
     Duck[] ducks = {new Duck("Jady", 10), new Duck("Woody", 20), new Duck("Malfy", 5), new Duck("Alfy", 15), new Duck("Bumfy", 35)};
     Arrays.sort(ducks);  
-    for (int i = 0; i < ducks.length; i++) {
-      System.out.println(ducks[i].name+"\t"+ducks[i].weight);
+    
+    display(ducks);
+    
+    Duck[] ducks2 = {new Duck("Myung", 10), new Duck("Sang", 5), new Duck("University", 15)};
+    
+    DuckComparator dc = new DuckComparator();
+    
+    Duck temp;
+    
+    for (int i = 0; i < ducks2.length - 1; i++) {
+      if (dc.compare(ducks2[i], ducks2[i+1]) > 0) {
+        temp = ducks2[i];
+        ducks2[i] = ducks2[i+1];
+        ducks2[i+1] = temp;
+      }
     }
     
+    display(ducks2);
+    
+  }
+  public static void display(Duck[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i].name+"\t"+arr[i].weight);
+    }
   }
 }
